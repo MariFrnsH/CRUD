0ccc1b77508a3b29d955a7abe621dcb5abaa8de8
MariFrnsH <marifers080@gmail.com>
2023-05-17 16:01:41 -0600

    agregando proyecto a git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Alumnos  } from "./components/Alumnos";
+function App() {
+  return (
+    <div className="App">
+      
+      <Alumnos />
+     
+    </div>
+  );
+}
+
+export default App;
